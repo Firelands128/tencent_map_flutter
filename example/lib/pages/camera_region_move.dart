3aea4c22c7a8d28a2e8028d5f7b8c733cdeeea5c
Wenqi Li <firelands128@gmail.com>
2024-04-24 16:48:01 +0800

    clean up code

--- example/lib/pages/camera_region_move.dart
+++ example/lib/pages/camera_region_move.dart
@@ -37,56 +37,58 @@ class _CameraRegionMovePageState extends State<CameraRegionMovePage> {
           ]),
         ],
       ),
-      body: Stack(children: [
-        TencentMap(
-          mapType: context.isDark ? MapType.dark : MapType.normal,
-          onMapCreated: (controller) => this.controller = controller,
-        ),
-        Positioned(
-          top: 20,
-          width: MediaQuery.of(context).size.width,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                child: const Text(' 视野 1 '),
-                onPressed: () => controller.moveCameraRegion(
-                  Region(
-                    north: 39.98437,
-                    east: 116.31863,
-                    south: 39.97837,
-                    west: 116.31363,
-                  ),
-                  EdgePadding(
-                    top: 0.2,
-                    right: 0.2,
-                    bottom: 0.2,
-                    left: 0.2,
-                  ),
-                  duration,
-                ),
-              ),
-              const SizedBox(width: 32),
-              ElevatedButton(
-                child: const Text(' 视野 2 '),
-                onPressed: () => controller.moveCameraToRegionWithPosition(
-                    [
-                      Position(latitude: 39.98437, longitude: 116.31863),
-                      Position(latitude: 39.98937, longitude: 116.32363),
-                      Position(latitude: 39.98037, longitude: 116.31163),
-                    ],
+      body: Stack(
+        children: [
+          TencentMap(
+            mapType: context.isDark ? MapType.dark : MapType.normal,
+            onMapCreated: (controller) => this.controller = controller,
+          ),
+          Positioned(
+            top: 20,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: const Text(' 视野 1 '),
+                  onPressed: () => controller.moveCameraRegion(
+                    Region(
+                      north: 39.98437,
+                      east: 116.31863,
+                      south: 39.97837,
+                      west: 116.31363,
+                    ),
                     EdgePadding(
                       top: 0.2,
                       right: 0.2,
                       bottom: 0.2,
                       left: 0.2,
                     ),
-                    duration),
-              ),
-            ],
+                    duration,
+                  ),
+                ),
+                const SizedBox(width: 32),
+                ElevatedButton(
+                  child: const Text(' 视野 2 '),
+                  onPressed: () => controller.moveCameraToRegionWithPosition(
+                      [
+                        Position(latitude: 39.98437, longitude: 116.31863),
+                        Position(latitude: 39.98937, longitude: 116.32363),
+                        Position(latitude: 39.98037, longitude: 116.31163),
+                      ],
+                      EdgePadding(
+                        top: 0.2,
+                        right: 0.2,
+                        bottom: 0.2,
+                        left: 0.2,
+                      ),
+                      duration),
+                ),
+              ],
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
